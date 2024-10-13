99e9dcd9a57962b15c36332f62ae91523cccb22d
Анисимов Алексей <mr.razor2706@gmail.com>
2024-10-13 14:06:48 +0300

    Сделано домашнее задание

--- hello.pas
+++ hello.pas
@@ -1,3 +1,20 @@
 ﻿begin
-  Print('Привет, мир!');
+  var lang := ReadString('Введите ваш язык: ');
+  if lang = 'Английский' then
+    Println('Hello!');
+  if lang = 'Русский' then
+    Println('Привет!');
+  if lang = 'Немецкий' then
+    Println('Hallo!');
 end.
+
+{
+Введите ваш язык:  Английский
+Hello!
+
+Введите ваш язык:  Русский
+Привет!
+
+Введите ваш язык:  Немецкий
+Hallo!
+}
